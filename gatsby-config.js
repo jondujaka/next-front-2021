af12bb5be1021dd4c016b48ae8386aa8d289ce2a
Jon Dujaka <jdujaka@gmail.com>
2021-05-06 13:14:52 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require('dotenv').config({
 });
 
 module.exports = {
-	pathPrefix: `/next-front-2021/public`,
+	pathPrefix: `/front/dist`,
 	siteMetadata: {
 		title: `Next 2021`,
 		description: `Next Festival 2021 Edition`,
