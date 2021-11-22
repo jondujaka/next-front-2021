c9e0f3c1959f31496ab4bff7096016b13846c7c1
Jon Dujaka <jon@my-clay.com>
2021-11-22 18:38:51 +0100

    privacy improvements

--- src/templates/privacy-policy.js
+++ src/templates/privacy-policy.js
@@ -18,9 +18,14 @@ const PrivacyPolicy = ({ pageContext }) => {
 					<h2 className="festival-page-title">Privacy Policy</h2>
 				</div>
 				<div className="col col-12 mt-5 mb-6">
-					<h1>Privacy Policy</h1>
 					<p>Effective date: 2021-11-22</p>
 					<p>Updated on: 2021-11-22</p>
+					<p>
+						<input id="csconsentcheckbox" type="checkbox" />
+						<label for="csconsentcheckbox">
+							I agree with Cookie Policy
+						</label>
+					</p>
 					<p>
 						This Privacy Policy explains the policies of Next
 						Festival on the collection and use of the information we
@@ -62,31 +67,34 @@ const PrivacyPolicy = ({ pageContext }) => {
 						information:
 					</p>
 
-					<ul>
-						<li>Usage Data</li>
-					</ul>
+					<p>
+						<ul>
+							<li>Usage Data</li>
+						</ul>
+					</p>
 
 					<p>Usage Data includes the following:</p>
-					<ul>
-						<li>
-							Internet Protocol (IP) address of computers
-							accessing the site
-						</li>
-						<li>Web page requests</li>
-						<li>Referring web pages</li>
-						<li>Browser used to access site</li>
-						<li>Time and date of access</li>
-					</ul>
-
+					<p>
+						<ul>
+							<li>
+								Internet Protocol (IP) address of computers
+								accessing the site
+							</li>
+							<li>Web page requests</li>
+							<li>Referring web pages</li>
+							<li>Browser used to access site</li>
+							<li>Time and date of access</li>
+						</ul>
+					</p>
 					<h4>How We Collect Information</h4>
 					<p>
 						https://nextfestival.sk collects and receives
 						information from you in the following manner:
+						<ul>
+							<li>When you interact with our Service.</li>
+							<li>From external, public sources.</li>
+						</ul>
 					</p>
-					<ul>
-						<li>When you interact with our Service.</li>
-						<li>From external, public sources.</li>
-					</ul>
 					<p>
 						Your information will be stored for up to 30 days after
 						it is no longer required to provide you the services.
@@ -100,21 +108,22 @@ const PrivacyPolicy = ({ pageContext }) => {
 					<p>
 						https://nextfestival.sk may use your information for the
 						following purposes:
+						<ul>
+							<li>
+								<b>Providing and maintaining our Service,</b> as
+								well as monitoring the usage of our Service.
+							</li>
+							<li>
+								<b>For other purposes.</b> Next Festival will
+								use your information for data analysis to
+								identify usage trends or determine the effective
+								of our marketing campaigns when reasonable. We
+								will use your information to evaluate and
+								improve our Service, products, services, and
+								marketing efforts.
+							</li>
+						</ul>
 					</p>
-					<ul>
-						<li>
-							<b>Providing and maintaining our Service,</b> as
-							well as monitoring the usage of our Service.
-						</li>
-						<li>
-							<b>For other purposes.</b> Next Festival will use
-							your information for data analysis to identify usage
-							trends or determine the effective of our marketing
-							campaigns when reasonable. We will use your
-							information to evaluate and improve our Service,
-							products, services, and marketing efforts.
-						</li>
-					</ul>
 
 					<h4>Third-party Sharing</h4>
 					<p>
@@ -129,20 +138,20 @@ const PrivacyPolicy = ({ pageContext }) => {
 					<p>
 						Your information may be shared to a third-party for
 						reasons including:
+						<ul>
+							<li>
+								<b>Analytics information.</b> Your information
+								might be shared with online analytics tools in
+								order to track and analyse website traffic.
+							</li>
+							<li>
+								<b>Payment processing and recovery services.</b>
+								Your information will be used in order to
+								process payments in the event of a purchase,
+								refund, or other similar request.
+							</li>
+						</ul>
 					</p>
-					<ul>
-						<li>
-							<b>Analytics information.</b> Your information might
-							be shared with online analytics tools in order to
-							track and analyse website traffic.
-						</li>
-						<li>
-							<b>Payment processing and recovery services.</b>
-							Your information will be used in order to process
-							payments in the event of a purchase, refund, or
-							other similar request.
-						</li>
-					</ul>
 					<p>
 						If you choose to provide such information during
 						registration or otherwise, you are giving Next Festival
@@ -153,21 +162,21 @@ const PrivacyPolicy = ({ pageContext }) => {
 					<p>
 						Your information may be disclosed for additional
 						reasons, including:
+						<ul>
+							<li>
+								Complying with applicable laws, regulations, or
+								court orders.
+							</li>
+							<li>
+								Responding to claims that your use of our
+								Service violates third-party rights.
+							</li>
+							<li>
+								Enforcing agreements you make with us, including
+								this Privacy Policy.
+							</li>
+						</ul>
 					</p>
-					<ul>
-						<li>
-							Complying with applicable laws, regulations, or
-							court orders.
-						</li>
-						<li>
-							Responding to claims that your use of our Service
-							violates third-party rights.
-						</li>
-						<li>
-							Enforcing agreements you make with us, including
-							this Privacy Policy.
-						</li>
-					</ul>
 
 					<h4>Cookies</h4>
 					<p>
@@ -179,21 +188,23 @@ const PrivacyPolicy = ({ pageContext }) => {
 						without your permission. All other cookies need to be
 						approved before they can be set in the browser.
 					</p>
-					<ul>
-						<li>
-							<b>Strictly necessary cookies.</b> Strictly
-							necessary cookies allow core website functionality
-							such as user login and account management. The
-							website cannot be used properly without strictly
-							necessary cookies.
-						</li>
-						<li>
-							<b>Performance cookies.</b> Performance cookies are
-							used to see how visitors use the website, eg.
-							analytics cookies. Those cookies cannot be used to
-							directly identify a certain visitor.
-						</li>
-					</ul>
+					<p>
+						<ul>
+							<li>
+								<b>Strictly necessary cookies.</b> Strictly
+								necessary cookies allow core website
+								functionality such as user login and account
+								management. The website cannot be used properly
+								without strictly necessary cookies.
+							</li>
+							<li>
+								<b>Performance cookies.</b> Performance cookies
+								are used to see how visitors use the website,
+								eg. analytics cookies. Those cookies cannot be
+								used to directly identify a certain visitor.
+							</li>
+						</ul>
+					</p>
 
 					<div class="cookie-report-container">
 						<p data-cookiereport="true">
@@ -234,7 +245,7 @@ const PrivacyPolicy = ({ pageContext }) => {
 					</p>
 					<p>Name: Next Festival</p>
 					<p>Address: Address, Bratislava, SK</p>
-					<p>Email: nfo@nextfestival.sk</p>
+					<p>Email: info(at)nextfestival.sk</p>
 					<p>Website: https://nextfestival.sk</p>
 				</div>
 			</Row>
